0dccda3be6bd867a86b188bcc75b3af3a4696c39
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-10-24 23:15:22 -0400

    Produce in season app

---
+++ public/main.js
@@ -0,0 +1,68 @@
+var save = document.getElementsByClassName("save");
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(save).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.querySelector('.produceName').innerText
+        const season = this.parentNode.childNodes[3].innerText
+        
+        fetch('save', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'season': season,
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+// Array.from(thumbDown).forEach(function(element) {
+//   element.addEventListener('click', function(){
+//     const name = this.parentNode.parentNode.childNodes[1].innerText
+//     const msg = this.parentNode.parentNode.childNodes[3].innerText
+//     const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//     fetch('messages', {
+//       method: 'put',
+//       headers: {'Content-Type': 'application/json'},
+//       body: JSON.stringify({
+//         'name': name,
+//         'msg': msg,
+//         'thumbUp':(thumbUp - 1) -1
+//       })
+//     })
+//     .then(response => {
+//       if (response.ok) return response.json()
+//     })
+//     .then(data => {
+//       console.log(data)
+//       window.location.reload(true)
+//     })
+//   });
+// });
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
